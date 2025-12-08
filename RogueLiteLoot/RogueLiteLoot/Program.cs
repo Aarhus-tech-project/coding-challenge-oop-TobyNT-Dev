de694611749371c8ae9bc5ee313c19e51d0f02f1
Tobias Thy <t.nt.dev.code@gmail.com>
2025-12-08 14:19:24 +0100

    Init

---
+++ RogueLiteLoot/RogueLiteLoot/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RogueLiteLoot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
